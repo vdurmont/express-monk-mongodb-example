d8bc1119fff1c671a21ad4fce982b6793d5755e3
Vincent DURMONT <vincent.durmont@ubleam.com>
2013-11-26 20:15:35 +0100

    Use of 'findAndModify' method from Monk + res.json instead of res.send for the errors

--- routes.js
+++ routes.js
@@ -6,7 +6,7 @@ var collection = db.get('bugs');
 // Returns all the bugs
 exports.getAll = function(req, res) {
 	collection.find({}, function(err, bugs){
-		if (err) res.send(500, err);
+		if (err) res.json(500, err);
 		else res.json(bugs);
 	});
 };
@@ -15,7 +15,7 @@ exports.getAll = function(req, res) {
 exports.create = function(req, res) {
 	var body = req.body;
 	collection.insert(body, function(err, bug){
-		if (err) res.send(500, err);
+		if (err) res.json(500, err);
 		else res.json(201, bug);
 	});
 };
@@ -24,24 +24,21 @@ exports.create = function(req, res) {
 exports.get = function(req, res) {
 	var id = req.params.id;
 	collection.findById(id, function(err, bug){
-		if (err) res.send(500, err);
+		if (err) res.json(500, err);
 		else if (bug) res.json(bug);
-		else res.send(404);
+		else res.json(404);
 	});
 };
 
 // Updates a bug
 exports.update = function(req, res) {
 	var id = req.params.id;
-	var bug = req.body;
-	delete bug._id;
-	collection.updateById(id, {$set: bug}, {safe:true, multi:false}, function(err, success){
-		if (err) res.send(500, err);
-		else if (success == 0) res.send(404);
-		else {
-			bug._id = id;
-			res.json(bug);
-		}
+	var body = req.body;
+	delete body._id;
+	collection.findAndModify({_id: id}, {$set: body}, {multi:false}, function(err, bug){
+		if (err) res.json(500, err);
+		else if (bug) res.json(bug);
+		else res.json(404);
 	});
 };
 
@@ -49,7 +46,7 @@ exports.update = function(req, res) {
 exports.del = function(req, res) {
 	var id = req.params.id;
 	collection.remove({_id: id}, function(err){
-		if (err) res.send(500, err);
+		if (err) res.json(500, err);
 		else res.json(204);
 	});
 };
